4a82364bb0f230220ccbeb3780e14257d0f316a9
Victor Stupitsyn <st.viktorr@gmail.com>
2024-10-23 22:21:21 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	js/main.js

--- js/main.js
+++ js/main.js
@@ -16,16 +16,16 @@ const maxLength = function (str, max) {
   return str.length > max;
 }
 
-// создаём данные 
+// создаём данные
 let dataArray = [];
 const names = ['Александр', 'Мария', 'Иван', 'Дарья', 'Максим', 'Екатерина', 'Артём', 'Анастасия', 'Дмитрий', 'Виктория'];
 const comments = 'Всё отлично! В целом всё неплохо. Но не всё. Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально. Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше. Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше. Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 
-//генерируем коммпентарии 
+//генерируем комментарии
 const getRandomComments = function (comments) {
   const commentsSplit = comments.split('. ');
   console.log(commentsSplit);
-  
+
   const count = commentsSplit.length;
   const commArray = [];
   for (let i = 0; i <= getRandom(1, count - 1); i++) {
@@ -35,7 +35,7 @@ const getRandomComments = function (comments) {
         avatar: `img/avatar-${getRandom(1, 6)}.svg`,
         message: comments.split('. ')[getRandom(1, comments.split('. ').length - 1)],
         name: names[getRandom(1, names.length - 1)],
-      }
+      },
     )
   }
   return commArray;
@@ -48,11 +48,10 @@ for (let i = 1; i <= 25; i++) {
       url: `photos/${i}.jpg`,
       description: 'В целом всё неплохо. Но не всё.',
       likes: getRandom(15, 200),
-      comments: getRandomComments(comments)
-    }
+      comments: getRandomComments(comments),
+    },
   )
 }
-console.log(dataArray)
 
 
 
